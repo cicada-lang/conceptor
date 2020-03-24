9b769c27e56c700c85d08b96821faf3ea0d8878e
Xie Yuheng <xyheme@gmail.com>
2020-03-24 16:04:06 +0800

    up

--- src/ctx/ctx-operator.ts
+++ src/ctx/ctx-operator.ts
@@ -1,6 +1,13 @@
 import * as Ctx from "./ctx"
 import * as util from "../util"
 
+// NOTE The extend of a set of attrs is the set of all objs that have all the attrs.
+// - when view a set of attrs as one attr,
+//   to say an obj has this attr,
+//   is to say the obj has each attr of the attrs.
+// - note that english does not have a mean
+//   to reverse the subject and the object of the verb `have`.
+//   unlike the word `own` can be reversed by `is owned by`.
 export function attrs_extent(
   ctx: Ctx.Ctx,
   attrs: Set<string>,
@@ -12,6 +19,10 @@ export function attrs_extent(
   return objs
 }
 
+// NOTE The intent of a set of objs is the set of all attrs that are shared by all objs.
+// - when view a set of objs as one obj,
+//   to say this obj has an attr,
+//   is to say each obj of the objs has the attr.
 export function objs_intent(
   ctx: Ctx.Ctx,
   objs: Set<string>,
