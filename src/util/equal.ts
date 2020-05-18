26ec9718c8e3fb95e453b2e88ddda25ffd335a67
Xie Yuheng <xyheme@gmail.com>
2020-05-18 23:51:09 +0800

    - fix `util.equal`

--- src/util/equal.ts
+++ src/util/equal.ts
@@ -4,9 +4,5 @@ import fast_deep_equal from "fast-deep-equal"
 // Comparison details of Node's `assert.deepEqual()`:
 //   https://nodejs.org/api/all.html#assert_assert_deepstrictequal_actual_expected_message
 export function equal(x: any, y: any): boolean {
-  if (typeof x.equal === 'function') {
-    return x.equal(y)
-  } else {
-    return fast_deep_equal(x, y)
-  }
+  return fast_deep_equal(x, y)
 }
