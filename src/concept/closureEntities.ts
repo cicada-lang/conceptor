d6c274391ad000478edd49f492c1e2015ab1ceaa
Xie Yuheng <xyheme@gmail.com>
2024-07-03 20:27:59 +0800

    `closureEntities`

--- src/concept/closureEntities.ts
+++ src/concept/closureEntities.ts
@@ -1,3 +1,13 @@
-// export function closureEntities(context: Context, ): {
+import {
+  commonAttributes,
+  commonEntities,
+  type Context,
+  type Entity,
+} from "../context/index.js"
 
-// }
+export function closureEntities(
+  context: Context,
+  entities: ReadonlySet<Entity>,
+): ReadonlySet<Entity> {
+  return commonEntities(context, commonAttributes(context, entities))
+}
