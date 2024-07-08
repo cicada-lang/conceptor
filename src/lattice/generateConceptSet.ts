492fe99fdf64bcfc5b8e765d21851b20a8aee4bc
Xie Yuheng <xyheme@gmail.com>
2024-07-08 21:15:08 +0800

    定义 `generateConceptSetByJoin` 然后 `generateConceptSet` 调用它

--- src/lattice/generateConceptSet.ts
+++ src/lattice/generateConceptSet.ts
@@ -1,27 +1,8 @@
-import { conceptJoin, type Concept } from "../concept/index.js"
+import { type Concept } from "../concept/index.js"
 import type { Context } from "../context/index.js"
 import type { QuotientSet } from "../utils/QuotientSet.js"
-import { generateAttributeConceptSet } from "./generateAttributeConceptSet.js"
-import { generateEntityConceptSet } from "./generateEntityConceptSet.js"
+import { generateConceptSetByJoin } from "./generateConceptSetByJoin.js"
 
 export function generateConceptSet(context: Context): QuotientSet<Concept> {
-  const targets = generateEntityConceptSet(context)
-  const results = generateAttributeConceptSet(context).union(targets)
-
-  while (true) {
-    const first = targets.representatives.shift()
-    if (first === undefined) {
-      break
-    }
-
-    for (const target of targets.representatives) {
-      const concept = conceptJoin(first, target)
-      if (!results.has(concept)) {
-        targets.add(concept)
-        results.add(concept)
-      }
-    }
-  }
-
-  return results
+  return generateConceptSetByJoin(context)
 }
